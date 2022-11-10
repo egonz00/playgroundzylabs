0778b51a60989a619cf3d30f65923a1ba5c2dd6e
egonz00 <117873344+egonz00@users.noreply.github.com>
2022-11-09 18:23:30 -0700

    Add files via upload

---
+++ c++/1.15 One large program/main 5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   
+   cout<<"Step 1 complete"<<endl;
+   cout<<"Step 2 as well"<<endl;
+   cout<<"All steps now complete"<<endl;
+
+   /* Type your code here. */
+
+   return 0;
+}
