504d36de1d4771ba8203331a81c41656a209b482
egonz00 <117873344+egonz00@users.noreply.github.com>
2022-11-09 18:20:22 -0700

    Update and rename main 4.cpp to main.cpp

--- c++/1.14  Interleaved input output/main.cpp
+++ c++/1.14  Interleaved input output/main.cpp
@@ -7,7 +7,9 @@ int main() {
    cout << "Enter x: " << endl;
    cin  >> x;
    
-    // Student mistakenly is echoing the input to output to match example
+   //cout << x << endl; // Student mistakenly is echoing the input to output to match example
+   
+   //solution
    cout << "x doubled is: " << 2 * x << endl;
    
    return 0;
