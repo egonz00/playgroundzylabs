02aee5ea0f9239a2fea400135f9ecc400101d8fe
egonz00 <117873344+egonz00@users.noreply.github.com>
2022-11-09 18:19:24 -0700

    Add files via upload

---
+++ c++/1.14  Interleaved input output/main 4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int x;
+
+   cout << "Enter x: " << endl;
+   cin  >> x;
+   
+    // Student mistakenly is echoing the input to output to match example
+   cout << "x doubled is: " << 2 * x << endl;
+   
+   return 0;
+}
